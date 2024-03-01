02c3e88a5c0e903f04aea43ab9ff402268893051
Savio110206 <savio.augusto2006@gmail.com>
2024-03-01 16:19:09 -0300

    criaçao do arquivo main.cpp
    durante va aula eu criei

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "sávio";
+
+            Console.WriteLine(nome);
+        }
+    }
+}
